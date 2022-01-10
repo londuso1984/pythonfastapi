fffed54f0e2dbd8ec1a5bc507ebbd059eb38fe59
Onduso <livingstoneonduso@gmail.com>
2022-01-10 10:37:39 +0300

    Modified user model to allow nulls on phn numbers

--- application/models.py
+++ application/models.py
@@ -27,7 +27,7 @@ class User(Base):
     __tablename__='users'
     id=Column(Integer,primary_key=True,nullable=False)
     email=Column(String, nullable=False, unique=True)
-    phone_number=Column(String,nullable=False)
+    phone_number=Column(String,nullable=True, server_default=text('0722988766987'))
     password=Column(String,nullable=False)
     created_at=Column(TIMESTAMP(timezone=True),nullable=False, server_default=text('now()'))
 
