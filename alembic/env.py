97ae39c276b970f41d451876aff9570be2d1c97b
Onduso <livingstoneonduso@gmail.com>
2022-01-10 09:15:52 +0300

    Modified env.py file

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from fastapi_project1.application.models import Base
-from fastapi_project1.application.config import settings
+from application.models import Base
+from application.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
