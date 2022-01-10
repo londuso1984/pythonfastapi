14e4f213c4d577c94d254d7c0a6550e9581d1e5a
Onduso <livingstoneonduso@gmail.com>
2022-01-10 09:29:10 +0300

    Modified versions file

---
+++ alembic/versions/18b8ef7ad0fe_add_all_other_columns_to_posts.py
@@ -0,0 +1,29 @@
+"""Add all other columns to posts
+
+Revision ID: 18b8ef7ad0fe
+Revises: dcdd5a2cc8be
+Create Date: 2022-01-07 12:43:21.125401
+
+"""
+from re import T
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '18b8ef7ad0fe'
+down_revision = 'dcdd5a2cc8be'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    #op.add_column('posts',sa.Column('published',sa.Boolean(),nullable=False,server_default="True"))
+    #op.add_column('posts',sa.Column('created_at', sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.text('now()')))
+    pass
+
+
+def downgrade():
+    # op.drop_column('posts','published')
+    #op.drop_column('posts', 'created_at')
+    pass
