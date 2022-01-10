0ff59d1032000973ca4d9c2b270d22bdb737c22e
Onduso <livingstoneonduso@gmail.com>
2022-01-10 09:19:56 +0300

    Modified versions file

--- alembic/versions/18b8ef7ad0fe_add_all_other_columns_to_posts.py
+++ alembic/versions/18b8ef7ad0fe_add_all_other_columns_to_posts.py
@@ -18,7 +18,7 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column('posts',sa.Column('published',sa.Boolean(),nullable=False,server_default=True))
+    op.add_column('posts',sa.Column('published',sa.Boolean(),nullable=False,server_default="True"))
     #op.add_column('posts',sa.Column('created_at', sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.text('now()')))
     pass
 
